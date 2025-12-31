0aeb2f9b9dab9e63124ad0280d328ddd265374eb
William-Almeida <williamkillerca@hotmail.com>
2025-12-31 13:13:18 -0300

    feat: Add `allowedDevOrigins` to Next.js configuration.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // @ts-ignore - allowedDevOrigins might not be in the type definition yet
+  allowedDevOrigins: ["mega.wmst.com.br"],
 };
 
 export default nextConfig;
